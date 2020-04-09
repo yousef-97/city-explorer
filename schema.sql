e6a4b874a8a2879c7f8c6f3ee420db1eba322750
yousef-97 <alshenyousef@gmail.com>
2020-04-09 22:25:46 +0300

    lab 8 Done

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE locations (
     id SERIAL PRIMARY KEY,
     search_query VARCHAR(255),
     formatted_query VARCHAR(255),
-    latitude VARCHAR(255),
-    longitude VARCHAR(255)
+    latitude NUMERIC(12,7),
+    longitude NUMERIC(12,7)
 );
 -- INSERT INTO locations (searchquery,latitude,longitude) VALUES ('amman',12345,123456);
